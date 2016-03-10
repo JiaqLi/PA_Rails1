b6116aeecc5ed25e6ecdcc9e739a66670a78672a
Jiaqi Li <jiaqli4@brandeis.edu>
2016-03-10 15:20:10 -0500

    db migrates and part of seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'json'
+
+json_courses = File.read('/home/jiaqi/Downloads/course.json')
+courses_obj = JSON.parse(json_courses)
+
+courses_obj.each do |course_record|
+  course_name = course_record['name']
+  course_id = course_record['subjects'][0].to_h['id']
+  #why there are multiple ids for one course?????
+  Course.create(name: course_name, subject_id: course_id)
+  #subject_id: course_record['subjects'][1]['id'])
+  #puts course_record['subjects'].size
+  #puts course_record['subjects'][1]
+  #puts "---------"
+  #puts course_record['subjects'][0].to_h['id']
+end
